a11e74506cd5255d6d4488cc5f33cc257ea0059c
GonzaloAriel <gonzaloarielacuna@gmail.com>
2024-01-28 12:11:21 -0300

    cards centradas, logo provisorio y nav corregida

--- js/proyectres.js
+++ js/proyectres.js
@@ -35,8 +35,8 @@ const agregar = (id) => {
   ticket.push(pelicula); //agregamos el producto al carrito
   localStorage.setItem("pelicula", JSON.stringify(ticket)); //guardamos como JSON en almacenamiento local.
 
-  const ticketEnStorage= JSON.parse(localStorage.getItem("pelicula"));//convertimos el archivo JSON en un array  
-  
+  const ticketEnStorage = JSON.parse(localStorage.getItem("pelicula"));//convertimos el archivo JSON en un array  
+
   console.log(ticketEnStorage);//control para ver si se paso bien el array.
 
 };
@@ -45,20 +45,21 @@ listaEventos.forEach((item) => {
   let div = document.createElement("div");
 
   div.innerHTML = `
-  <div class="card" style="width: 12rem;">
-            <img src="${item.img}" class="card-img-top" alt="iamgen-${item.id}">
-            <div class="card-body">
-                <p class="card-text">Aca descripcion de la pelicula o sinopsis</p>
-            </div>
-  </div>
-  <h2> ${item.nombre}
-  <b> ${item.precio}$
-  <button class="boton_comprar" id="boton${item.id}"> Comprar </button>
+    <div class="card" style="width: 12rem;">
+      <img src="${item.img}" class="card-img-top" alt="iamgen-${item.id}">
+      <div class="card-body">
+        <p class="card-text">Aca descripcion de la pelicula o sinopsis</p>
+      </div>
+        <h2 class="titulo_card"> ${item.nombre}
+        <b class="titulo_card"> ${item.precio}$
+        <button class="boton_comprar" id="boton${item.id}"> Comprar
+        </button>
+    </div>
   `;
   contenido.append(div)
-  
- let boton = document.getElementById(`boton${item.id}`);
- boton.addEventListener("click", () => agregar(item.id));
+
+  let boton = document.getElementById(`boton${item.id}`);
+  boton.addEventListener("click", () => agregar(item.id));
 });
 
 
